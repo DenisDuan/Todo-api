5d34a553d29a8ec3ff9a2055744a101ad00b8b7b
Denis Duan <denisduan@gmail.com>
2016-07-30 23:17:25 +1000

    add some test data for to do collection

--- server.js
+++ server.js
@@ -6,9 +6,32 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// TODO collections
+var todos = [{
+    id: 1,
+    description: 'Shopping',
+    completed: false
+}, {
+    id: 2,
+    description: 'Play Pokemon go',
+    completed: false
+},{
+    id: 3,
+    description: 'Call mum',
+    completed: true
+}];
+
+
 app.get('/', function (req, res) {
     res.send('Todo API Root');
 });
+
+// GET /todos : get all todos
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+// GET /todo/:id get a single todo
+
 
 app.listen(PORT, function () {
     console.log('Express listening on port: ' + PORT +'!');
