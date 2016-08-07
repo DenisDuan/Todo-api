0df46908e6fa720192e017022175f6cf3a84a9db
Denis Duan <denisduan@gmail.com>
2016-08-08 00:59:37 +1000

    Add comments for configuring postgres

--- db.js
+++ db.js
@@ -1,3 +1,24 @@
+/**
+ * Steps to get postgres database running on Heroku
+ * 
+ * 1. Install addon on Heroku:
+ *    heroku addons:create heroku-postgresql:hobby-dev
+ * 
+ * 2. Wait for Heroku finishes installing pg
+ *    heroku pg:wait
+ * 
+ * 3. Install postgres on the project:
+ *    npm install pg@4.4.1 --save
+ * 
+ * 4. Install postgres hstore module:
+ *    npm install pg-hstore@2.3.2 --save
+ * 
+ * 5. Configure the sequelize configruation file using environment variable. e.g. shown in this file 
+ * 
+ * 6. Push the changes to Heroku
+ *    
+ */
+
 var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
